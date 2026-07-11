0ccd1e494ae23a39bf71ae9c5f7c90398af956ec
SanjanaChintawar <sanjanachintawar007@gmail.com>
2026-07-11 23:13:35 +0530

    First Unique Character in a String

---
+++ FirstUniqueCharacterinaString/charString.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+class Solution{
+    public int solve(String s){
+        HashMap<Character, Integer> map = new HashMap<>();
+        char[] arr = s.toCharArray();
+        for(char ch : arr){
+            map.put(ch, map.getOrDefault(ch , 0)+1);
+        }
+        for(int i = 0; i < arr.length; i++){
+            if(map.get(arr[i]) == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+public class charString {
+    public static void main(String[] args) {
+        
+        Solution s = new Solution();
+        System.out.println(s.solve("sansjana"));
+    }
+}
